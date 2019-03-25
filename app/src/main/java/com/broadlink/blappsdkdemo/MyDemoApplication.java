88b033e22af011f613fa47279fa3b8acdb20415f
daydreamnist <651369843@outlook.com>
2019-03-25 17:10:36 +0800

    build project

---
+++ app/src/main/java/com/broadlink/blappsdkdemo/MyDemoApplication.java
@@ -0,0 +1,8 @@
+package com.broadlink.blappsdkdemo;
+
+import android.app.Application;
+
+public class MyDemoApplication extends Application {
+
+
+}
---
+++ app/src/main/java/com/broadlink/blappsdkdemo/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.broadlink.blappsdkdemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.broadlink.blappsdkdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
